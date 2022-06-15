d411c53456870e500885a5fb08aa4dad448835b4
lelinrashed <lelin.rashed784@gmail.com>
2022-06-15 23:52:57 +0600

    add block variable

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,8 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var username string = "rashed"
-	rashed := "lelin"
-	fmt.Printf(username)
-	fmt.Println(rashed)
+
+	// Variable types in golang
+	// int
+	// float32
+	// string
+	// bool
+
+	// In golang we can define variable through two different way
+	// 1. with var keyword
+	// var variableName type = value
+	// 2. Another way to use ":=" this sign to define variable
+	// variableName := value ( In this case type will be inferred by the value )
+
+	// var username string = "rashed"
+	// rashed := "lelin"
+	// fmt.Printf(username)
+	// fmt.Println(rashed)
+
+	var (
+		a int
+		b int    = 1
+		c string = "hello"
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
