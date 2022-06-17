8f19d68e80a43d13a395e10063bb19ea8a16d16e
lelinrashed <lelin.rashed784@gmail.com>
2022-06-17 22:24:18 +0600

    add node js backend server for api

--- 20urls/main.go
+++ 20urls/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
+
+const myUrl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj3435"
 
 func main() {
 	fmt.Println("Welcome to handling URLs to golang")
+	fmt.Println(myUrl)
+
+	//
+	result, _ := url.Parse(myUrl)
+
+	fmt.Println(result.Scheme)
+
 }
